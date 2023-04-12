6e5cce88e5a479b75c4412b59173a7990203e8c4
KaranCS14 <hamsithacskg2ipsvig@gmail.com>
2023-04-12 17:16:04 +0530

    add some small bug fixes

--- seed.js
+++ seed.js
@@ -16,73 +16,7 @@ client
   })
   .catch((ex) => {
     winston.error(ex)
-  })
-
-// (async function seed() {
-//   await client.query(
-//     `
-//     CREATE TABLE Employees(
-//         id SERIAL PRIMARY KEY,
-//         name VARCHAR(55),
-//         email VARCHAR(75),
-//         password VARCHAR(255),
-//         education VARCHAR(75),
-//         age SMALLINT,
-//         isAdmin BOOL
-//     )
-    
-//     CREATE TABLE Departments(
-//         id SERIAL PRIMARY KEY,
-//         employee_id INTEGER REFERENCES Employees(id),
-//         name
-//         position VARCHAR(55)
-//     )
-//     CREATE TABLE Statuses(
-//       id SERIAL PRIMARY KEY,
-//       performance_id INTEGER REFERENCES Performances(id),
-//       name VARCHAR(30)
-//   )
-
-//     CREATE TABLE Performances(
-//       id SERIAL PRIMARY KEY,
-//       status_id INTEGER REFERENCES Statuses(id),
-//       employee_id INTEGER REFERENCES Employees(id)
-//   )
-    
-//   CREATE TABLE Levels(
-//     id SERIAL PRIMARY KEY,
-//     employee_id INTEGER REFERENCES Employees(id),
-//     name VARCHAR(35)
-// )
-
-//     CREATE TABLE Skills(
-//         id SERIAL PRIMARY KEY,
-//         employee_id INTEGER REFERENCES Employees(id),
-//         name VARCHAR(55),
-//         level_id INTEGER REFERENCES Levels(id)
-//     )
-    
-//     CREATE TABLE Benefits(
-//         id SERIAL PRIMARY KEY,
-//         employee_id INTEGER REFERENCES Employees(id),
-//         name VARCHAR(5),
-//         package VARCHAR(35)
-//     )
-    
-//     CREATE TABLE Experiences(
-//         id SERIAL PRIMARY KEY,
-//         employee_id INTEGER REFERENCES Employees(id),
-//         company VARCHAR(55),
-//         "From" DATE,
-//         "To" DATE
-//     )
-    
-//     `,
-//     []
-//   )
-// })()
-
-// IIFE function, not recommend
+  });
 
 async function germinate() {
   await client.query(
