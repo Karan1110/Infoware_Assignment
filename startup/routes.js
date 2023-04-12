2b602be7f0e1c46ccb2667996e154d6850aa30dc
KaranCS14 <hamsithacskg2ipsvig@gmail.com>
2023-04-12 11:16:32 +0530

    complete adding relations

--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const users = require("../routes/users");
+const benefits = require("../routes/benefits");
+const departments = require("../routes/departments");
+const experiences = require("../routes/experiences");
+const statuses = require("../routes/statuses");
+const levels = require("../routes/levels");
+const skills = require("../routes/skills");
 const error = require("../middlewares/error");
 const db = require("../middlewares/connectWithDB");
 
@@ -8,6 +14,13 @@ module.exports = function (app) {
     app.use(express.json());
     app.use(db);
     app.use("/users", users);
+    app.use("/benefits", benefits);
+    app.use("/departments", departments);
+    app.use("/experiences", experiences);
+    app.use("/levels", levels);
+    app.use("/performances", performances);
+    app.use("/skills", skills);
+    app.use("/statuses", statuses);
     app.use(error);
 }
 
