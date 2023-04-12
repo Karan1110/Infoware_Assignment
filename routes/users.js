e146ce451e3aca5be7a08f8ab7d528da27ea5275
KaranCS14 <hamsithacskg2ipsvig@gmail.com>
2023-04-12 13:57:37 +0530

    small fixes

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const auth = require("../middlewares/auth");
 const isAdmin = require("../middlewares/isAdmin");
 
-router.get("/me",[auth],async (req, res) => {
+router.get("/me",[auth,isAdmin],async (req, res) => {
     const id = req.user.id;
     const { rows } = await req.db.query(`
     SELECT *
@@ -25,7 +25,7 @@ router.get("/me",[auth],async (req, res) => {
         res.status(200).send(rows);
     });
 
-router.get("/:id" ,[auth],async (req, res) => {
+router.get("/:id" ,[auth,isAdmin],async (req, res) => {
    const {rows} =  await req.db.query(`
    SELECT *
    FROM Employees e
@@ -44,7 +44,7 @@ router.get("/:id" ,[auth],async (req, res) => {
     res.status(200).send(rows);
 });
 
-router.get("/",  [auth],async (req, res) => {
+router.get("/",  [auth,isAdmin],async (req, res) => {
    const {rows} =  await req.db.query(`
    SELECT *
    FROM Employees e
@@ -64,7 +64,7 @@ router.get("/",  [auth],async (req, res) => {
     res.status(200).send(rows);
 });
 
-router.post("/", auth,[auth],async (req, res) => {
+router.post("/", auth,[auth,isAdmin],async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const p = await bcrypt.hash(req.body.password, salt);
 
@@ -87,7 +87,7 @@ router.post("/", auth,[auth],async (req, res) => {
       res.status(200).send(req.user);
   });
 
-router.put("/:id" ,auth,[auth],async (req, res) => {
+router.put("/:id" ,auth,[auth,isAdmin],async (req, res) => {
     const { rows } = await req.db.query(`
 UPDATE Employees
 SET name = $1,
@@ -115,7 +115,7 @@ RETURNING *
 });
 
 
-router.delete("/:id" , auth,[auth],async (req, res) => {
+router.delete("/:id" , auth,[auth,isAdmin],async (req, res) => {
     await req.db.query(`
 DELETE FROM Employees
 WHERE id = $1;
