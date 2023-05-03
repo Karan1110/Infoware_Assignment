b55f51510c1d5709ce0d9bc4b73876b6241a19c1
KaranCS14 <hamsithacskg2ipsvig@gmail.com>
2023-05-03 21:52:47 +0530

    add prisma implementation to the routes

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,8 @@ const auth = require("../middlewares/auth");
 const isAdmin = require("../middlewares/isAdmin");
 const email_verified = require("../middlewares/isMailCode");
 const config = require("config")
-const {prisma} = require("../startup/db")
+const {prisma} = require("../startup/db");
+const { memoryStorage } = require("multer");
 
 router.get("/average_salary",[auth,isAdmin],async (req, res) => {
     const avg_salary = prisma.employee.aggregate({
@@ -18,8 +19,12 @@ router.get("/average_salary",[auth,isAdmin],async (req, res) => {
 });
 
 router.get("/me", [auth, isAdmin], async (req, res) => {
-    prisma.employee.findMany({
+    const me = prisma.employee.find({
+        where: {
+           id  :req.user.id
+       },
         include: {
+            manager : true,
             education: true,
             experience: true,
             notification: true,
@@ -36,147 +41,76 @@ router.get("/me", [auth, isAdmin], async (req, res) => {
             performance_status  : true
 
         }
-    })
-    res.status(200).send({ "employee details": rows[0] });
+   });
+    
+    res.status(200).send(me);
 });
     
 router.get("/:id", [auth, isAdmin], async (req, res) => {
-    
-    const { rows } = await req.db.query(`
-    SELECT 
-        e.id,
-        e.name,
-        e.email,
-        e.phone,
-        e.salary,
-        e.password,
-        e.education,
-        e.age,
-        e.isadmin,
-        s.name AS performance_status,
-        d.name AS department,
-        d.position AS position,
-        array_agg(sk.name) AS skills,
-        array_agg(l.name) as skill_level,
-        ex.company,
-        b.package
-      FROM Employees e
-      JOIN Performances p ON p.employee_id = e.id 
-      JOIN Statuses s ON p.status_id = s.id
-      JOIN Benefits b ON b.employee_id = e.id
-      JOIN Departments d ON d.employee_id = e.id
-      JOIN Experiences ex ON ex.employee_id = e.id
-      JOIN Skills sk ON sk.employee_id = e.id
-      JOIN Levels l ON sk.level_id = l.id
-      WHERE e.id = $1
-      GROUP BY e.id,
-      e.name, 
-      e.email, 
-      e.phone,
-      e.salary,
-      e.password, 
-      e.education, 
-      e.age, 
-      e.isadmin, 
-      s.name,
-      p.employee_id, 
-      d.name,
-      d.position, 
-      ex.company, 
-      b.package;
-    `,
-        [
-            req.params.id
-        ]);
-    res.status(200).send(rows[0]);
+    const employee = prisma.employee.find({
+        where: {
+           id  :req.params.id
+       },
+        include: {
+            manager : true,
+            education: true,
+            experience: true,
+            notification: true,
+            ticket: true,
+            skills: true,
+            level: true,
+            benefit: true,
+            benefit_type: true,
+            meeting: true,
+            meeting_member: true,
+            department: true,
+            position: true,
+            performance: true,
+            performance_status  : true
+
+        }
+   });
+    res.status(200).send(employee);
 });
 
 router.get("/",  [auth,isAdmin],async (req, res) => {
-   const {rows} =  await req.db.query(`
-   SELECT 
-   e.id,
-   e.name,
-   e.email,
-   e.phone,
-   e.salary,
-   e.password,
-   e.education,
-   e.age,
-   e.isadmin,
-   s.name AS performance_status,
-   d.name AS department,
-   d.position AS position,
-   array_agg(sk.name) AS skills,
-   array_agg(l.name) as skill_level,
-   ex.company,
-   b.package
+    const employee = prisma.employee.findMany({
+        include: {
+            manager : true,
+            education: true,
+            experience: true,
+            notification: true,
+            ticket: true,
+            skills: true,
+            level: true,
+            benefit: true,
+            benefit_type: true,
+            meeting: true,
+            meeting_member: true,
+            department: true,
+            position: true,
+            performance: true,
+            performance_status  : true
 
- FROM Employees e
-
- JOIN Performances p ON p.employee_id = e.id 
- JOIN Statuses s ON p.status_id = s.id
- JOIN Benefits b ON b.employee_id = e.id
- JOIN Departments d ON d.employee_id = e.id
- JOIN Experiences ex ON ex.employee_id = e.id
- JOIN Skills sk ON sk.employee_id = e.id
- JOIN Levels l ON sk.level_id = l.id
-
- GROUP BY e.id,
- e.name, 
- e.email, 
- e.phone,
- e.salary,
- e.password, 
- e.education, 
- e.age, 
- e.isadmin, 
- s.name,
- p.employee_id, 
- d.name,
- d.position, 
- ex.company, 
- b.package;
-LIMIT $1 OFFSET $2
-    `,
-        [
-            req.query.l,
-            req.query.s
-        ]);
-    res.status(200).send(rows);
+        }
+    });
+    
+    res.status(200).send(employee);
 });
 
 router.post("/", email_verified, async (req, res) => {
     
+    const userExists = prisma.employee.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
 
-    const { ifExists } = await req.db.query(
-        `
-    SELECT EXISTS (SELECT * FROM Employees WHERE name = $1 OR email = $2 OR phone = $3);
-  `, [
-        req.body.name,
-        req.body.email,
-            req.body.phone,
-    ]);
-
-    if (ifExists) return res.status(400).send("User already exists.");
+    if (userExists) return res.status(400).send("User already exists.");
 
     const salt = await bcrypt.genSalt(10);
     const p = await bcrypt.hash(req.body.password, salt);
 
-    const {rows} = await req.db.query(`
-        SELECT * FROM create_employee($1,$2,$3,$4,$5,$6,$7,$8);
-  `, [
-        req.body.name,
-        req.body.email,
-        req.body.phone,
-        req.body.salary,
-        p,
-        req.body.education,
-        req.body.age,
-        req.body.isAdmin
-      ]);
-      
-      const { id,name, email, education, age, isAdmin } = rows[0];
-    req.user = { id: id, isAdmin: isAdmin };
 
     const token = jwt.sign({ id: id, isAdmin: isAdmin }, config.get("jwtPrivateKey"));
       
--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,6 @@
      function connectDB() {
     const { PrismaClient } = require('@prisma/client');
-     const prisma = new PrismaClient();
+     const prisma = new PrismaClient({datasources : process.env.DATABASE_URL});
          return prisma;
 };
 
