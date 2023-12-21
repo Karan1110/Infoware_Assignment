7882bceaceb4b574d3a2188b44e40d5abae07568
Karan1110 <karancsipsvig5@gmail.com>
2023-12-21 18:53:10 +0530

    resolve config issues

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,6 @@
-const Sequelize = require("sequelize");
-const config = require("config");
+const Sequelize = require("sequelize")
+const config = require("config")
 
 module.exports = new Sequelize(config.get("dbURL"), {
-    logging: false
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> 9403bcbb26062a45a5fea260b94d47eeeeb2aaa7
+  logging: false,
+})
